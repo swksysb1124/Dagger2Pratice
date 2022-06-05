20f7f2b7d6ed24d951116cadb2193b3627a57fb2
Jason1 Su (蘇暐凱_Askey_TW) <jason1_su@askey.com>
2022-06-06 07:17:23 +0800

    use @Module & @Binds for user local data source

--- app/src/main/java/studio/wksjason/dagger2pratice/UserRepository.kt
+++ app/src/main/java/studio/wksjason/dagger2pratice/UserRepository.kt
@@ -1,11 +1,12 @@
 package studio.wksjason.dagger2pratice
 
+import dagger.Binds
 import dagger.Component
 import dagger.Module
 import dagger.Provides
 import javax.inject.Inject
 
-@Component(modules = [UserRemoteDataSourceModule::class])
+@Component(modules = [UserRemoteDataSourceModule::class, UserLocalDataSourceModule::class])
 interface UserRepositoryComponent {
     fun inject(activity: MainActivity)
 }
@@ -18,14 +19,20 @@ class UserRepository @Inject constructor(
 }
 
 @Module
-class UserRemoteDataSourceModule {
+abstract class UserRemoteDataSourceModule {
+
+    @Binds
+    abstract fun provideUserApiService(advancedUserSelfDefinedApiService: AdvancedUserSelfDefinedApiService): UserApiService
+}
+
+@Module
+class UserLocalDataSourceModule {
 
     @Provides
-    fun provideUserApiService(apiClient3rdPartyLib: ApiClient3rdPartyLib): UserApiService =
-        User3rdPartyApiService(apiClient3rdPartyLib)
-
-    @Provides
-    fun provide3rdPartyLib(): ApiClient3rdPartyLib = ApiClient3rdPartyLib()
+    fun provideUser3rdPartyDatabase(): UserDatabase = User3rdPartyDatabase.Builder()
+        .name("user database")
+        .size(1000)
+        .build()
 }
 
 
@@ -41,7 +48,13 @@ interface UserApiService {
 
 }
 
-class UserSelfDefinedApiService : UserApiService {
+class AdvancedUserSelfDefinedApiService @Inject constructor() : UserSelfDefinedApiService() {
+    init {
+        println("AdvancedUserSelfDefinedApiService object initialize")
+    }
+}
+
+open class UserSelfDefinedApiService @Inject constructor() : UserApiService {
     init {
         println("UserSelfDefinedApiService object initialize")
     }
@@ -59,6 +72,33 @@ class ApiClient3rdPartyLib {
     }
 }
 
-class UserDatabase @Inject constructor() {
+interface UserDatabase {
 
 }
+
+class User3rdPartyDatabase(builder: Builder) : UserDatabase {
+
+    init {
+        println("User3rdPartyDatabase object initialize")
+    }
+
+    val name: String?
+    val size: Int?
+
+    init {
+        this.name = builder.name
+        this.size = builder.size
+    }
+
+    class Builder {
+        var name: String? = null
+            private set
+        var size: Int? = null
+            private set
+
+        fun name(name: String) = apply { this.name = name }
+        fun size(size: Int) = apply { this.size = size }
+        fun build() = User3rdPartyDatabase(this)
+    }
+}
+
