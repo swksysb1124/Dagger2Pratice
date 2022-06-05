fd0143eac95c5b5b5472e11098610ae76887f7f7
Jason1 Su (蘇暐凱_Askey_TW) <jason1_su@askey.com>
2022-06-06 01:10:43 +0800

    use @Module & @Provides for user remote data source

--- app/src/main/java/studio/wksjason/dagger2pratice/UserRepository.kt
+++ app/src/main/java/studio/wksjason/dagger2pratice/UserRepository.kt
@@ -1,9 +1,11 @@
 package studio.wksjason.dagger2pratice
 
 import dagger.Component
+import dagger.Module
+import dagger.Provides
 import javax.inject.Inject
 
-@Component
+@Component(modules = [UserRemoteDataSourceModule::class])
 interface UserRepositoryComponent {
     fun inject(activity: MainActivity)
 }
@@ -15,6 +17,18 @@ class UserRepository @Inject constructor(
 
 }
 
+@Module
+class UserRemoteDataSourceModule {
+
+    @Provides
+    fun provideUserApiService(apiClient3rdPartyLib: ApiClient3rdPartyLib): UserApiService =
+        User3rdPartyApiService(apiClient3rdPartyLib)
+
+    @Provides
+    fun provide3rdPartyLib(): ApiClient3rdPartyLib = ApiClient3rdPartyLib()
+}
+
+
 class UserLocalDataSource @Inject constructor(val userDatabase: UserDatabase) {
 
 }
@@ -23,8 +37,26 @@ class UserRemoteDataSource @Inject constructor(val userApiService: UserApiServic
 
 }
 
-class UserApiService @Inject constructor() {
+interface UserApiService {
 
+}
+
+class UserSelfDefinedApiService : UserApiService {
+    init {
+        println("UserSelfDefinedApiService object initialize")
+    }
+}
+
+class User3rdPartyApiService(val apiClient3rdPartyLib: ApiClient3rdPartyLib) : UserApiService {
+    init {
+        println("User3rdPartyApiService object initialize")
+    }
+}
+
+class ApiClient3rdPartyLib {
+    init {
+        println("ApiClient3rdPartyLib object initialize")
+    }
 }
 
 class UserDatabase @Inject constructor() {
