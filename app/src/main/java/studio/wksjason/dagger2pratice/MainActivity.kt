6f7e0b6d470609b7713227978575d8894712bed5
Jason1 Su (蘇暐凱_Askey_TW) <jason1_su@askey.com>
2022-06-05 23:57:14 +0800

    project init

---
+++ app/src/main/java/studio/wksjason/dagger2pratice/MainActivity.kt
@@ -0,0 +1,11 @@
+package studio.wksjason.dagger2pratice
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
